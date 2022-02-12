8f5bd50c28a602ea6e7cc994e86a4bc626caf782
Billy McPherson <billymcfly@gmail.com>
2022-02-11 20:35:27 -0700

    Populate grid with list of files/folders from folder

--- ShowFolderNotifyIcon/MainWindow.xaml.cs
+++ ShowFolderNotifyIcon/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,30 +21,80 @@ namespace ShowFolderNotifyIcon
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IFileSystem _fileSystem;
+
         public const int TaskbarHeight = 44;
 
         #region Constructors
-        public MainWindow()
+
+        public MainWindow(IFileSystem fileSystem)
         {
+            _fileSystem = fileSystem;
+
             InitializeComponent();
-            var startPos = GetWindowStartPos();
-            this.Left = startPos.X;
-            this.Top = startPos.Y;
+            Loaded += MainWindow_Loaded;
         }
 
         #endregion
 
+        // Most methods are public, as to be unit testable
         #region Public Methods
+
         /// <summary>
         /// Get the position the window should be in
         /// </summary>
-        // Public, as to be unit testable
         public Point GetWindowStartPos()
         {
             return new Point(
                     System.Windows.SystemParameters.PrimaryScreenWidth - this.Width,
                     System.Windows.SystemParameters.PrimaryScreenHeight - this.Height - TaskbarHeight
                 );
+        }
+
+        public List<string> GetFileList()
+        {
+            const string path = "D:\\documents\\QA stuff";
+            var directories = _fileSystem.Directory.GetDirectories(path);
+            var files = _fileSystem.Directory.GetFiles(path);
+
+            return directories.Concat(files).ToList();
+        }
+
+        // For unit testing
+        public Grid GetMainGrid()
+        {
+            return mainGrid;
+        }
+
+        public void PopulateGrid(List<string> files)
+        {
+            foreach(var file in files)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+                var fileTextBox = new TextBox();
+                fileTextBox.Text = file;
+                fileTextBox.SetValue(Grid.RowProperty, mainGrid.RowDefinitions.Count - 1);
+                fileTextBox.SetValue(Grid.ColumnProperty, 0);
+                fileTextBox.Background = new SolidColorBrush(Color.FromRgb(19, 19, 19));
+                fileTextBox.FontSize = 16;
+                fileTextBox.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                mainGrid.Children.Add(fileTextBox);
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        // So we can have "clean" unit tests that aren't calling a bunch of crazy methods because everything's in the constructor
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var startPos = GetWindowStartPos();
+            this.Left = startPos.X;
+            this.Top = startPos.Y;
+
+            var files = GetFileList();
+            PopulateGrid(files);
         }
 
         #endregion
--- ShowFolderNotifyIconTest/MainWindowTests.cs
+++ ShowFolderNotifyIconTest/MainWindowTests.cs
@@ -1,5 +1,9 @@
+using AutoFixture;
+using Moq;
 using NUnit.Framework;
 using ShowFolderNotifyIcon;
+using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Threading;
 
 namespace ShowFolderNotifyIconTest
@@ -7,16 +11,25 @@ namespace ShowFolderNotifyIconTest
     [Apartment(ApartmentState.STA)]
     public class MainWindowTests
     {
+        private Fixture _fixture;
+
+        public MainWindowTests()
+        {
+            _fixture = new Fixture();
+        }
+
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
-        public void GetWindowStartPos()
+        public void GetWindowStartPos_HappyPath()
         {
             // Arrange
-            var mainWindow = new MainWindow();
+            var mockFileSystem = new Mock<IFileSystem>();
+            var mainWindow = new MainWindow(mockFileSystem.Object);
             var expectedX = System.Windows.SystemParameters.PrimaryScreenWidth - mainWindow.Width;
             var expectedY = System.Windows.SystemParameters.PrimaryScreenHeight - mainWindow.Height - MainWindow.TaskbarHeight;
 
@@ -26,6 +39,89 @@ namespace ShowFolderNotifyIconTest
             // Assert
             Assert.AreEqual(expectedX, startPos.X);
             Assert.AreEqual(expectedY, startPos.Y);
+        }
+
+        [Test]
+        public void GetFileList_HappyPath()
+        {
+            // Arrange
+            var mockFiles = new string[]
+            {
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>()
+            };
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(x => x.Directory.GetDirectories(It.IsAny<string>())).Returns(mockFiles);
+
+            var mainWindow = new MainWindow(mockFileSystem.Object);
+
+            // Act
+            var resultFiles = mainWindow.GetFileList();
+
+            // Assert
+            for(int i = 0; i < resultFiles.Count; i++)
+            {
+                Assert.AreEqual(mockFiles[i], resultFiles[i]);
+            }
+        }
+
+        [Test]
+        public void GetFileList_NoFiles()
+        {
+            // Arrange
+            var mockFiles = new string[] { };
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(x => x.Directory.GetDirectories(It.IsAny<string>())).Returns(mockFiles);
+
+            var mainWindow = new MainWindow(mockFileSystem.Object);
+
+            // Act
+            var resultFiles = mainWindow.GetFileList();
+
+            // Assert
+            Assert.AreEqual(0, resultFiles.Count);
+        }
+
+        [Test]
+        public void PopulateGrid_HappyPath()
+        {
+            // Arrange
+            var mockFiles = new List<string>()
+            {
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>()
+            };
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            var mainWindow = new MainWindow(mockFileSystem.Object);
+
+            // Act 
+            mainWindow.PopulateGrid(mockFiles);
+
+            // Assert
+            Assert.AreEqual(mockFiles.Count, mainWindow.GetMainGrid().RowDefinitions.Count);
+        }
+
+        [Test]
+        public void PopulateGrid_NoFiles()
+        {
+            // Arrange
+            var mockFiles = new List<string>() { };
+
+            var mockFileSystem = new Mock<IFileSystem>();
+            var mainWindow = new MainWindow(mockFileSystem.Object);
+
+            // Act 
+            mainWindow.PopulateGrid(mockFiles);
+
+            // Assert
+            Assert.AreEqual(0, mainWindow.GetMainGrid().RowDefinitions.Count);
         }
     }
 }
